6e9e9897108c68ef02b4cc5cb5774d9171a7eec7
David Nguyen <brown87434@gmail.com>
2022-08-02 23:35:06 -0500

    grid divs do not stack when creating grid size and can now reset grid size to 0 to create new grid

--- eas.js
+++ eas.js
@@ -1,12 +1,14 @@
 const content = document.querySelector(".content");
 const sizeBtn = document.querySelector(".btn-size");
 const clearBtn = document.querySelector(".btn-clear");
+const resetBtn = document.querySelector(".btn-reset");
 
 // event listener to set size of grid
 sizeBtn.addEventListener("click", createGrid);
 
 // event listener to clear the grid
 clearBtn.addEventListener("click", clearGrid);
+resetBtn.addEventListener("click", resetGrid);
 
 // function to create grid
 function createGrid() {
@@ -19,22 +21,23 @@ function createGrid() {
 
   for (let i = 0; i < sizeSelection * sizeSelection; i++) {
     const grid = document.createElement("div");
-    grid.classList.add("square");
-
+    // grid.classList.add("square");
     grid.addEventListener("mouseover", function () {
       grid.style.backgroundColor = "black";
     });
-
     content.appendChild(grid);
   }
 }
 
-function resetGrid() {}
+function resetGrid() {
+  const content = document.querySelector(".content");
+  while (content.firstChild) content.removeChild(content.firstChild);
+}
 
 function clearGrid() {
   let squareDivs = content.getElementsByTagName("div");
-  console.log("LENGTH IS " + squareDivs.length);
-  console.log(squareDivs);
+  // console.log("LENGTH IS " + squareDivs.length);
+  // console.log(squareDivs); returns a node list
 
   for (let i = 0; i < squareDivs.length; i++) {
     squareDivs[i].style.backgroundColor = "#d3d3d3";
